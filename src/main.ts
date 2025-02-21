69c2fa0c0cf8c01635294b2abbc4c408c0ad9a2e
linzeqin <1351123861@qq.com>
2025-02-21 10:08:47 +0800

    ✨ feat: 增加 跨域 CORS 支持

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // 启动 CORS
+  app.enableCors();
+  
   await app.listen(3000);
 }
 bootstrap();
